624c022e2fe6d491fff9a242973e3557dcf76825
Gábor Barát <barat.gabor@gmail.com>
2020-10-02 21:19:02 +0200

    Add own StringSegment implementation

---
+++ src/GridOS/Utility/StringSegment.cs
@@ -0,0 +1,44 @@
+﻿namespace IngameScript
+{
+    partial class Program
+    {
+        public struct StringSegment
+        {
+            public readonly int Start;
+            public readonly int Length;
+
+            private string _string;
+            private string _stringSegmentCache;
+
+            public StringSegment(string text)
+            {
+                _string = text;
+                Start = 0;
+                Length = text.Length;
+                _stringSegmentCache = null;
+            }
+
+            public StringSegment(string text, int start, int length)
+            {
+                _string = text;
+                Start = start;
+                Length = length;
+                _stringSegmentCache = null;
+            }
+
+            public bool IsEmpty => _string == null;
+            public bool IsCached => _stringSegmentCache != null;
+
+            public override string ToString()
+            {
+                if (_string == null)
+                    return null;
+
+                if (_stringSegmentCache != null)
+                    return _stringSegmentCache;
+
+                return _stringSegmentCache = _string.Substring(Start, Length);
+            }
+        }
+    }
+}
