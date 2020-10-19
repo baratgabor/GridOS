46e0b2592e687d8bcf958796ec341ea4f701fc13
Gábor Barát <barat.gabor@gmail.com>
2020-10-19 16:15:02 +0200

    Remove last unecessary nesting. Good grief.

--- src/GridOS/Core/DisplaySystem/DisplayInfrastructure/DisplayController.cs
+++ src/GridOS/Core/DisplaySystem/DisplayInfrastructure/DisplayController.cs
@@ -2,89 +2,86 @@ using System;
 
 namespace IngameScript
 {
-    partial class Program
+    /// <summary>
+    /// Knows how to instantiate and wire together the full stack of classes needed for a working view system.
+    /// Sets up and mediates the execution of display related commands.
+    /// </summary>
+    class DisplayController : IDisposable, IDisplayContext
     {
-        /// <summary>
-        /// Knows how to instantiate and wire together the full stack of classes needed for a working view system.
-        /// Sets up and mediates the execution of display related commands.
-        /// </summary>
-        class DisplayController : IDisposable, IDisplayContext
+        public string Name { get; }
+
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IDiagnosticService _diagnostics;
+        private readonly IView _view;
+        private readonly IGlobalEvents _globalEvents;
+        private readonly Breadcrumb _breadcrumb;
+        private readonly Menu _menu;
+
+        private readonly CommandItem _upCommand;
+        private readonly CommandItem _downCommand;
+        private readonly CommandItem _selectCommand;
+
+        public DisplayController(string name, ICommandDispatcher commandDispatcher, MainConfig config, IDiagnosticService diagnostics, IView view, IMenuGroup menuRoot, IGlobalEvents globalEvents)
         {
-            public string Name { get; }
+            Name = name;
+            _view = view;
+            _diagnostics = diagnostics;
+            _commandDispatcher = commandDispatcher;
+            _globalEvents = globalEvents;
 
-            private readonly ICommandDispatcher _commandDispatcher;
-            private readonly IDiagnosticService _diagnostics;
-            private readonly IView _view;
-            private readonly IGlobalEvents _globalEvents;
-            private readonly Breadcrumb _breadcrumb;
-            private readonly Menu _menu;
+            _globalEvents.ExecutionWillFinish += DisplayTick;
 
-            private readonly CommandItem _upCommand;
-            private readonly CommandItem _downCommand;
-            private readonly CommandItem _selectCommand;
+            _upCommand = new CommandItem($"{Name}Up", OnMoveUp);
+            _downCommand = new CommandItem($"{Name}Down", OnMoveDown);
+            _selectCommand = new CommandItem($"{Name}Select", OnSelect);
 
-            public DisplayController(string name, ICommandDispatcher commandDispatcher, MainConfig config, IDiagnosticService diagnostics, IView view, IMenuGroup menuRoot, IGlobalEvents globalEvents)
-            {
-                Name = name;
-                _view = view;
-                _diagnostics = diagnostics;
-                _commandDispatcher = commandDispatcher;
-                _globalEvents = globalEvents;
+            _menu = new Menu(
+                new MenuModel(menuRoot, this),
+                config,
+                new TextSurfaceWordWrapper(config));
+            _breadcrumb = new Breadcrumb(config);
+            _menu.NavigationPathChanged += _breadcrumb.OnPathChanged;
 
-                _globalEvents.ExecutionWillFinish += DisplayTick;
+            _view
+                .AddControl(new DisplayHeader(config, _diagnostics))
+                .AddControl(_breadcrumb)
+                .AddControl(_menu);
 
-                _upCommand = new CommandItem($"{Name}Up", OnMoveUp);
-                _downCommand = new CommandItem($"{Name}Down", OnMoveDown);
-                _selectCommand = new CommandItem($"{Name}Select", OnSelect);
+            _commandDispatcher
+                .AddCommand_OverwriteExisting(_upCommand)
+                .AddCommand_OverwriteExisting(_downCommand)
+                .AddCommand_OverwriteExisting(_selectCommand);
 
-                _menu = new Menu(
-                    new MenuModel(menuRoot, this),
-                    config,
-                    new TextSurfaceWordWrapper(config));
-                _breadcrumb = new Breadcrumb(config);
-                _menu.NavigationPathChanged += _breadcrumb.OnPathChanged;
-
-                _view
-                    .AddControl(new DisplayHeader(config, _diagnostics))
-                    .AddControl(_breadcrumb)
-                    .AddControl(_menu);
-
-                _commandDispatcher
-                    .AddCommand_OverwriteExisting(_upCommand)
-                    .AddCommand_OverwriteExisting(_downCommand)
-                    .AddCommand_OverwriteExisting(_selectCommand);
-
-                _menu.PushUpdate();
-            }
-
-            public void Dispose()
-            {
-                _globalEvents.ExecutionWillFinish -= DisplayTick;
-                _commandDispatcher.RemoveCommand(_upCommand);
-                _commandDispatcher.RemoveCommand(_downCommand);
-                _commandDispatcher.RemoveCommand(_selectCommand);
-            }
-
-            private void OnMoveUp(CommandItem _, string __)
-                => _menu.MoveUp();
-
-            private void OnMoveDown(CommandItem _, string __)
-                => _menu.MoveDown();
-
-            private void OnSelect(CommandItem _, string __)
-                => _menu.Select();
-
-            private void DisplayTick()
-                => _view.Redraw();
-
-            private void SetFontSize(float fontSize)
-                => _view.SetFontSize(fontSize);
-
-            private void SetFontColor(Color color)
-                => _view.SetFontColor(color);
-
-            private void SetBackgroundColor(Color color)
-                => _view.SetBackgroundColor(color);
+            _menu.PushUpdate();
         }
+
+        public void SetFontSize(float fontSize)
+            => _view.SetFontSize(fontSize);
+
+        public void SetFontColor(Color color)
+            => _view.SetFontColor(color);
+
+        public void SetBackgroundColor(Color color)
+            => _view.SetBackgroundColor(color);
+
+        public void Dispose()
+        {
+            _globalEvents.ExecutionWillFinish -= DisplayTick;
+            _commandDispatcher.RemoveCommand(_upCommand);
+            _commandDispatcher.RemoveCommand(_downCommand);
+            _commandDispatcher.RemoveCommand(_selectCommand);
+        }
+
+        private void OnMoveUp(CommandItem _, string __)
+            => _menu.MoveUp();
+
+        private void OnMoveDown(CommandItem _, string __)
+            => _menu.MoveDown();
+
+        private void OnSelect(CommandItem _, string __)
+            => _menu.Select();
+
+        private void DisplayTick()
+            => _view.Redraw();
     }
 }
