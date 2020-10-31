a6e62966d5de38f99865be3e547dbc3564de1902
Gábor Barát <barat.gabor@gmail.com>
2020-10-31 22:59:54 +0100

    Change default colors

--- src/GridOS/Core/Configuration/StaticConfig.cs
+++ src/GridOS/Core/Configuration/StaticConfig.cs
@@ -8,22 +8,27 @@ namespace IngameScript
 
         public static IReadOnlyList<ColorSetting> FontColors = new List<ColorSetting>()
         {
-            new ColorSetting() { Name = "Cyan", Color = Color.Multiply(Color.Cyan, 0.9f) },
+            new ColorSetting() { Name = "Cyan", Color = Color.Cyan },
             new ColorSetting() { Name = "White", Color = Color.Multiply(Color.White, 0.9f) },
             new ColorSetting() { Name = "Yellow", Color = Color.Multiply(Color.Yellow, 0.9f) },
             new ColorSetting() { Name = "Black", Color = Color.Black },
-            new ColorSetting() { Name = "Crimson", Color = Color.Multiply(Color.Crimson, 0.9f) },
-            new ColorSetting() { Name = "Navy", Color = Color.Multiply(Color.Navy, 0.9f) },
-            new ColorSetting() { Name = "Orchid", Color = Color.Multiply(Color.DarkOrchid, 0.9f) },
+            new ColorSetting() { Name = "Crimson", Color = Color.Crimson },
+            new ColorSetting() { Name = "Navy", Color = Color.Navy },
+            new ColorSetting() { Name = "Orchid", Color = Color.DarkOrchid },
+            new ColorSetting() { Name = "Wheat", Color = Color.Wheat},
+            new ColorSetting() { Name = "Keen 1", Color = new Color(186, 238, 249) },
+            new ColorSetting() { Name = "Keen 2", Color = new Color(198, 218, 226) },
         };
 
         public static IReadOnlyList<ColorSetting> BackgroundColors = new List<ColorSetting>()
         {
             new ColorSetting() { Name = "Black", Color = Color.Black },
-            new ColorSetting() { Name = "Cyan", Color = Color.Darken(Color.Cyan, 0.4f) },
-            new ColorSetting() { Name = "Crimson", Color = Color.Darken(Color.Crimson, 0.4f) },
+            new ColorSetting() { Name = "Cyan", Color = Color.Darken(Color.Cyan, 0.7f) },
+            new ColorSetting() { Name = "Crimson", Color = Color.Darken(Color.Crimson, 0.5f) },
             new ColorSetting() { Name = "Navy", Color = Color.Darken(Color.Navy, 0.4f) },
-            new ColorSetting() { Name = "Orchid", Color = Color.Darken(Color.DarkOrchid, 0.4f) },
+            new ColorSetting() { Name = "Orchid", Color = Color.Darken(Color.DarkOrchid, 0.5f) },
+            new ColorSetting() { Name = "Keen 1", Color = new Color(43, 54, 61) },
+            new ColorSetting() { Name = "Keen 2", Color = new Color(85, 109, 117) },
         };
 
         public struct ColorSetting
