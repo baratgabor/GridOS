831985f638e7b0d8fd6cdf1e9866f449b74234d4
Gábor Barát <barat.gabor@gmail.com>
2020-11-06 23:12:29 +0100

    Remove incorrect type constraint from grid service and improve method naming

--- src/GridOS/Utility/GridService.cs
+++ src/GridOS/Utility/GridService.cs
@@ -1,53 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace IngameScript
 {
     interface IGridService
     {
-        void GetAll<T>(List<T> listToPopulate, Func<IMyTerminalBlock, bool> predicate = null) where T : class, IMyTerminalBlock;
-        List<T> GetAll<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class, IMyTerminalBlock;
+        void GetAllOfType<T>(List<T> listToPopulate, Func<T, bool> predicate = null) where T : class;
+        IEnumerable<T> GetAllOfType<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class;
+        IEnumerable<IMyTerminalBlock> GetAllTerminalBlocksOfType<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class;
+        void GetAllTerminalBlocksOfType<T>(List<IMyTerminalBlock> listToPopulate, Func<IMyTerminalBlock, bool> predicate = null) where T : class;
         IMyTerminalBlock GetByName(string name);
-        T GetFirst<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class, IMyTerminalBlock;
-        T GetFirstOrDefault<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class, IMyTerminalBlock;
+        T GetFirst<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class;
+        T GetFirstOrDefault<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class;
     }
 
     class GridService : IGridService
     {
         private readonly IMyGridTerminalSystem _gts;
-        private readonly List<IMyTerminalBlock> _blockBuffer;
+        private readonly List<IMyTerminalBlock> _blockBuffer = new List<IMyTerminalBlock>();
 
         public GridService(IMyGridTerminalSystem gts)
         {
             _gts = gts;
         }
 
-        public T GetFirst<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class, IMyTerminalBlock
+        public T GetFirst<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class
         {
             _blockBuffer.Clear();
             _gts.GetBlocksOfType<T>(_blockBuffer, predicate);
             return (T)_blockBuffer.First();
         }
 
-        public T GetFirstOrDefault<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class, IMyTerminalBlock
+        public T GetFirstOrDefault<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class
         {
             _blockBuffer.Clear();
             _gts.GetBlocksOfType<T>(_blockBuffer, predicate);
             return (T)_blockBuffer.FirstOrDefault();
         }
 
-        public void GetAll<T>(List<T> listToPopulate, Func<IMyTerminalBlock, bool> predicate = null) where T : class, IMyTerminalBlock
+        public void GetAllOfType<T>(List<T> listToPopulate, Func<T, bool> predicate = null) where T : class
+        {
+            _gts.GetBlocksOfType(listToPopulate, predicate);
+        }
+
+        public IEnumerable<T> GetAllOfType<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class
+        {
+            _blockBuffer.Clear();
+            _gts.GetBlocksOfType<T>(_blockBuffer, predicate);
+
+            foreach (var item in _blockBuffer)
+            {
+                yield return (T)item;
+            }
+        }
+
+        public void GetAllTerminalBlocksOfType<T>(List<IMyTerminalBlock> listToPopulate, Func<IMyTerminalBlock, bool> predicate = null) where T : class
         {
             _gts.GetBlocksOfType<T>(listToPopulate, predicate);
         }
 
-        public List<T> GetAll<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class, IMyTerminalBlock
+        public IEnumerable<IMyTerminalBlock> GetAllTerminalBlocksOfType<T>(Func<IMyTerminalBlock, bool> predicate = null) where T : class
         {
-            var blockList = new List<T>();
-            _gts.GetBlocksOfType<T>(blockList, predicate);
-            return blockList;
+            _blockBuffer.Clear();
+            _gts.GetBlocksOfType<T>(_blockBuffer, predicate);
+            return _blockBuffer;
         }
 
         public IMyTerminalBlock GetByName(string name)
